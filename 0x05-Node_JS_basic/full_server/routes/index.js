f62b31ba1b54824243e4f4038eb55419b0254b32
JThungu <josephkangethe848@gmail.com>
2024-02-27 15:07:55 +0300

    files and README

---
+++ 0x05-Node_JS_basic/full_server/routes/index.js
@@ -0,0 +1,13 @@
+const AppController = require('../controllers/AppController');
+const StudentsController = require('../controllers/StudentsController');
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/', AppController.getHomepage);
+
+router.get('/students', StudentsController.getAllStudents);
+
+router.get('/students/:major', StudentsController.getAllStudentsByMajor);
+
+module.exports = router;
